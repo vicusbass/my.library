36e570d6c6f165e0cb84187d448413403ba64738
vicus <vasile.pop@gmail.com>
2018-01-16 00:08:32 +0200

    cleanup useless comments

--- src/login.php
+++ src/login.php
@@ -1,10 +1,8 @@
 <?php
 require('../config/Db.php');
-// Define variables and initialize with empty values
 $email = $password = $idadmin = "";
 $email_err = $password_err = "";
 
-// Processing form data when form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Check if email is empty
@@ -23,29 +21,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Validate credentials
     if (empty($email_err) && empty($password_err)) {
-        // Prepare a select statement
         $sql = "SELECT email, password, isadmin, id, first_name, last_name FROM users WHERE email = ?";
 
         if ($stmt = mysqli_prepare($dbc, $sql)) {
-            // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "s", $param_email);
 
-            // Set parameters
             $param_email = $email;
 
-            // Attempt to execute the prepared statement
             if (mysqli_stmt_execute($stmt)) {
                 // Store result
                 mysqli_stmt_store_result($stmt);
 
                 // Check if email exists, if yes then verify password
                 if (mysqli_stmt_num_rows($stmt) == 1) {
-                    // Bind result variables
                     mysqli_stmt_bind_result($stmt, $email, $hashed_password, $isadmin, $id, $first_name, $last_name);
                     if (mysqli_stmt_fetch($stmt)) {
                         if (password_verify($password, $hashed_password)) {
-                            /* Password is correct, so start a new session and
-                            save the email to the session */
+                            /* Password is correct, so start a new session and save the email to the session */
                             session_start();
                             $_SESSION['email'] = $email;
                             $_SESSION['isadmin'] = $isadmin;
@@ -71,7 +63,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             }
         }
 
-        // Close statement
         mysqli_stmt_close($stmt);
     }
 
--- src/register.php
+++ src/register.php
@@ -8,19 +8,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty(trim($_POST["email"]))) {
         $email_err = "Please enter an email.";
     } else {
-        // Prepare a select statement
         $sql = "SELECT id FROM users WHERE email = ?";
 
         if ($stmt = mysqli_prepare($dbc, $sql)) {
-            // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "s", $param_email);
 
-            // Set parameters
             $param_email = trim($_POST["email"]);
 
-            // Attempt to execute the prepared statement
             if (mysqli_stmt_execute($stmt)) {
-                /* store result */
                 mysqli_stmt_store_result($stmt);
 
                 if (mysqli_stmt_num_rows($stmt) == 1) {
@@ -33,11 +28,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             }
         }
 
-        // Close statement
         mysqli_stmt_close($stmt);
     }
 
-    // Validate password
     if (empty(trim($_POST['password']))) {
         $password_err = "Please enter a password.";
     } elseif (strlen(trim($_POST['password'])) < 6) {
@@ -46,7 +39,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $password = trim($_POST['password']);
     }
 
-    // Validate confirm password
     if (empty(trim($_POST["confirm_password"]))) {
         $confirm_password_err = 'Please confirm password.';
     } else {
@@ -56,14 +48,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     }
 
-    // Validate first name
     if (empty(trim($_POST["first_name"]))) {
         $first_name_err = 'Please add first name.';
     } else {
         $first_name = trim($_POST['first_name']);
     }
 
-    // Validate last name
     if (empty(trim($_POST["last_name"]))) {
         $last_name_err = 'Please add last name.';
     } else {
@@ -73,22 +63,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Check input errors before inserting in database
     if (empty($email_err) && empty($password_err) && empty($confirm_password_err) && empty($first_name_err) && empty($last_name_err)) {
 
-        // Prepare an insert statement
         $sql = "INSERT INTO users (email, password, first_name, last_name) VALUES (?, ?, ?, ?)";
 
         if ($stmt = mysqli_prepare($dbc, $sql)) {
-            // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "ssss", $param_email, $param_password, $param_first_name, $param_last_name);
 
-            // Set parameters
             $param_email = $email;
-            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            $param_password = password_hash($password, PASSWORD_DEFAULT);
             $param_first_name = $first_name;
             $param_last_name = $last_name;
 
-            // Attempt to execute the prepared statement
             if (mysqli_stmt_execute($stmt)) {
-                // Redirect to login page
                 header("location: login.php");
             } else {
                 die('stmt error: ' . mysqli_stmt_error($stmt));
@@ -98,11 +83,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
 
 
-        // Close statement
         mysqli_stmt_close($stmt);
     }
 
-    // Close connection
     mysqli_close($dbc);
 }
 ?>
